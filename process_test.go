09b40387f81765a046bde801e12fa2acd8334103
Enes Furkan Olcay <fopeak@gmail.com>
2020-07-15 18:51:43 +0300

    process_test

--- process_test.go
+++ process_test.go
@@ -1,40 +1,55 @@
 package baraka
 
 import (
-	"mime/multipart"
 	"net/http"
 	"strings"
 	"testing"
 )
 
+type HeaderTest struct {
+	header http.Header
+	parser Parser
+}
+
 func TestStore(t *testing.T) {
+	tests := []HeaderTest{
+		{http.Header{"Content-Type": {`multipart/form-data; boundary="MyBoundary"`}}, WithMultipartReader{}},
+		{http.Header{"Content-Type": {`text/plain`}}, WithMultipartReader{}},
+		{http.Header{"Content-Type": {`multipart/form-data; boundary="MyBoundary"`}}, WithParseMultipartForm{}},
+		{http.Header{"Content-Type": {`text/plain`}}, WithParseMultipartForm{}},
+	}
+
 	raw := `
 --MyBoundary
 Content-Disposition: form-data; name="filea"; filename="filea.txt"
 Content-Type: text/plain
-	
+
 test file
 --MyBoundary--
 `
-	s, err := NewStorage("./", WithMultipartReader{
-		Filter: func(data *multipart.Part) bool {
-			return true
-		},
-	})
-	if err != nil {
-		t.Error(err)
-	}
-	b := strings.NewReader(strings.ReplaceAll(raw, "\n", "\r\n"))
-	req, _ := http.NewRequest("POST", "http://localhost", b)
-	req.Header = http.Header{"Content-Type": {`multipart/form-data; boundary="MyBoundary"`}}
+	for _, test := range tests {
+		s, err := NewStorage("./", test.parser)
+		if err != nil {
+			t.Error(err)
+		}
+		b := strings.NewReader(strings.ReplaceAll(raw, "\n", "\r\n"))
+		req, _ := http.NewRequest("POST", "http://localhost", b)
+		req.Header = test.header
 
-	p, err := s.Parse(req)
-	if err != nil {
-		t.Error(err)
+		p, err := s.Parse(req)
+		if err != nil {
+			// expects specified error if content type is text/plain
+			if err == http.ErrNotMultipart && test.header["Content-Type"][0] == "text/plain" {
+				continue
+			}
+
+			t.Fatal(err)
+		}
+
+		err = p.Store()
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 
-	err = p.Store()
-	if err != nil {
-		t.Error(err)
-	}
 }
