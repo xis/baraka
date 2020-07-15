8f001c9cf9cd8a63700cafa23009ac38a7d2e518
Enes Furkan Olcay <fopeak@gmail.com>
2020-07-16 01:17:33 +0300

    header comments

--- header.go
+++ header.go
@@ -6,7 +6,8 @@ import (
 	"net/textproto"
 )
 
-// Header @
+// Header is a similar to multipart.FileHeader
+// defined it again in here because can't access the unexported field of the multipart.Header
 type Header struct {
 	Filename string
 	Header   textproto.MIMEHeader
@@ -14,7 +15,8 @@ type Header struct {
 	content  []byte
 }
 
-// Open @
+// Open is a similar to multipart.FileHeader.Open function
+// opens Header
 func (fh *Header) Open() *io.SectionReader {
 	r := io.NewSectionReader(bytes.NewReader(fh.content), 0, int64(len(fh.content)))
 	return r
