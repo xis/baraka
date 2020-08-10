e6fd1bd54cb2e9ce718920cfbcd1199f18128f42
Enes Furkan Olcay <fopeak@gmail.com>
2020-08-10 18:01:21 +0300

    new struct Parts for all file purposes

--- saver.go
+++ saver.go
@@ -2,69 +2,28 @@ package baraka
 
 import (
 	"io"
-	"mime/multipart"
 	"os"
+	"strconv"
 )
 
 // Saver is a interface for determine which data type to store
 // Parse interface functions returns this interface
 type Saver interface {
-	Save(path string) error
+	Save(prefix string, path string) error
 }
 
-// MultipartForm implements the Saver interface
-type MultipartForm struct {
-	files multipart.Form
-	// MultipartForm filter function runs when saving file
-	filter func(data multipart.File) bool
-}
-
-// MultipartParts implements the Saver interface
-type MultipartParts struct {
+// Parts implements the Saver interface
+type Parts struct {
 	files []*Header
+	len   int
 }
 
 // Save is a method for saving multipart files.
-// this method saves multipart.Form data
-// will be used when WithParseMultipartForm passed to Storage
-func (s MultipartForm) Save(path string) error {
-	for _, multiparts := range s.files.File {
-		for _, multipart := range multiparts {
-			file, err := multipart.Open()
-			defer file.Close()
-			if err != nil {
-				return err
-			}
-			// execute filter function
-
-			if s.filter != nil {
-				okay := s.filter(file)
-				if !okay {
-					continue
-				}
-			}
-
-			out, err := os.Create(path + multipart.Filename)
-			defer out.Close()
-			if err != nil {
-				return err
-			}
-			_, err = io.Copy(out, file)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-// Save is a method for saving multipart files.
-// this method saves MultipartParts data.
-// will be used when WithMultipartReader passed to Storage
-func (s MultipartParts) Save(path string) error {
-	for _, header := range s.files {
+// this method saves Parts data.
+func (s *Parts) Save(prefix string, path string) error {
+	for i, header := range s.files {
 		f := header.Open()
-		out, err := os.Create(path + header.Filename)
+		out, err := os.Create(path + prefix + strconv.Itoa(i))
 		defer out.Close()
 		if err != nil {
 			return err
--- saver_test.go
+++ saver_test.go
@@ -1,52 +1,13 @@
 package baraka
 
 import (
-	"io"
-	"mime/multipart"
 	"os"
 	"syscall"
 	"testing"
 )
 
-func TestSave_MultipartForm(t *testing.T) {
-	s, err := NewStorage("./", WithParseMultipartForm{})
-	if err != nil {
-		t.Error(err)
-	}
-	req := CreateRequest(RawMultipartPlainText)
-	p, err := s.Parse(req)
-	if err != nil {
-		t.Error(err)
-	}
-
-	form := p.saver.(MultipartForm)
-
-	form.filter = func(data multipart.File) bool {
-		return false
-	}
-
-	path := "./"
-	err = form.Save(path)
-	if err != nil && err != io.EOF {
-		t.Error(err)
-	}
-	form.filter = func(data multipart.File) bool {
-		return true
-	}
-	path = ""
-	for _, form := range form.files.File {
-		for _, file := range form {
-			file.Filename = ""
-		}
-	}
-	err = form.Save(path)
-	if err != nil && err.(*os.PathError).Err != syscall.ENOENT {
-		t.Error(err)
-	}
-}
-
 func TestSave_MultipartParts(t *testing.T) {
-	s, err := NewStorage("./", WithMultipartReader{})
+	s, err := NewStorage("./", Options{})
 	if err != nil {
 		t.Error(err)
 	}
@@ -55,13 +16,13 @@ func TestSave_MultipartParts(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	mp := p.saver.(MultipartParts)
-	err = mp.Save("./")
+	mp := p.saver.(*Parts)
+	err = mp.Save("test", "./")
 	if err != nil {
 		t.Error(err)
 	}
 	mp.files[0].Filename = ""
-	err = mp.Save("")
+	err = mp.Save("test", "")
 	if err != nil && err.(*os.PathError).Err != syscall.ENOENT {
 		t.Error(err)
 	}
