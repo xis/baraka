f00ae98cdad353e282a631abdd7369eac8999317
xis <xis_xi@aol.com>
2021-01-30 22:08:24 +0300

    added FileSystemStore changed the parameters of the Save function created tests for the new things on the Saver

--- saver.go
+++ saver.go
@@ -1,6 +1,55 @@
 package baraka
 
+import (
+	"os"
+	"path/filepath"
+)
+
 // Saver is a interface that wraps the Save function
 type Saver interface {
-	Save(prefix string, path string, excludedContentTypes ...string) error
+	Save(path string, filename string, part *Part) error
+}
+
+// FileSystemStore is the default store to save parts
+type FileSystemStore struct {
+	Path string
+}
+
+// NewFileSystemStore creates a new FileSystemStore
+func NewFileSystemStore(path string) FileSystemStore {
+	return FileSystemStore{
+		Path: path,
+	}
+}
+
+// Save is a method for saving parts into disk.
+func (s FileSystemStore) Save(path string, filename string, part *Part) error {
+	if !isDir(path) {
+		err := os.MkdirAll(path, os.ModeSticky|os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+	extension := filepath.Ext(part.Name)
+	out, err := os.Create(filepath.Join(path, filename+extension))
+	defer out.Close()
+	if err != nil {
+		return err
+	}
+	_, err = out.Write(part.Content)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// helper functions
+
+// isDir checks if file exists or not
+func isDir(path string) bool {
+	f, e := os.Stat(path)
+	if e != nil {
+		return false
+	}
+	return f.IsDir()
 }
