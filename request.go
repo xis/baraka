079c2e45cd6d54ecf9af0b393f2da802a2feb9d4
xis <xis_xi@aol.com>
2021-01-31 21:37:51 +0300

    removed all functions added GetForm method parts field inside Request struct is a map now added tests for the new things

--- request.go
+++ request.go
@@ -1,124 +1,26 @@
 package baraka
 
-import (
-	"mime"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
-)
+import "errors"
 
 // Request implements the Processor interface.
 // contains an array of parts.
 // parser.Parse() returns Request as Processor.
 type Request struct {
-	parts []*Part
+	parts map[string][]*Part
 }
 
 // NewRequest creates a new Request with parts inside
-func NewRequest(parts ...*Part) *Request {
+func NewRequest(parts map[string][]*Part) *Request {
 	return &Request{
 		parts,
 	}
 }
 
-// Save is a method for saving parts into disk.
-func (s *Request) Save(prefix string, folderPath string, excludedContentTypes ...string) error {
-	if !isDir(folderPath) {
-		err := os.MkdirAll(folderPath, os.ModeSticky|os.ModePerm)
-		if err != nil {
-			return err
-		}
+// GetForm returns the parts of the requested form, returns error if form does not exists
+func (r *Request) GetForm(formname string) ([]*Part, error) {
+	form, ok := r.parts[formname]
+	if !ok {
+		return nil, errors.New("form not found")
 	}
-	for key := range s.parts {
-		file := s.parts[key]
-		if len(excludedContentTypes) != 0 {
-			excluded := isExcluded(file.Headers.Get("Content-Type"), excludedContentTypes...)
-			if excluded {
-				continue
-			}
-		}
-		extension := filepath.Ext(file.Name)
-		out, err := os.Create(filepath.Join(folderPath, prefix+strconv.Itoa(key)+extension))
-		defer out.Close()
-		if err != nil {
-			return err
-		}
-		_, err = out.Write(file.Content)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// Content method gives you []*Part, so you can access all the data of the parts.
-func (s *Request) Content() []*Part {
-	return s.parts
-}
-
-// Length returns length of the parts
-func (request *Request) Length() int {
-	return len(request.parts)
-}
-
-// Filenames returns filenames of the parts
-func (request *Request) Filenames() []string {
-	filenames := make([]string, len(request.parts))
-	for k, v := range request.parts {
-		filenames[k] = v.Name
-	}
-	return filenames
-}
-
-// ContentTypes returns content types of the parts
-func (request *Request) ContentTypes() []string {
-	contentTypes := make([]string, len(request.parts))
-	for k, v := range request.parts {
-		contentTypes[k] = v.Headers.Get("Content-Type")
-	}
-	return contentTypes
-}
-
-// GetJSON returns json strings of the application/json parts separately
-func (request *Request) GetJSON() ([]string, error) {
-	jsons := []string{}
-	for _, file := range request.parts {
-		if file.Size == 0 {
-			continue
-		}
-		media, _, err := mime.ParseMediaType(file.Headers.Get("Content-Type"))
-		if err != nil {
-			return nil, err
-		}
-		if media == "application/json" {
-			jsons = append(jsons, string(file.Content))
-		}
-	}
-	return jsons, nil
-}
-
-// helper functions
-
-// isExcluded returns that if file's content type is on excluded content type list or not.
-func isExcluded(contentType string, excludedContentTypes ...string) bool {
-	for key := range excludedContentTypes {
-		i := strings.Index(contentType, ";")
-		if i == -1 {
-			i = len(contentType)
-		}
-		contentType = strings.TrimSpace(strings.ToLower(contentType[0:i]))
-		if contentType == excludedContentTypes[key] {
-			return true
-		}
-	}
-	return false
-}
-
-func isDir(path string) bool {
-	f, e := os.Stat(path)
-	if e != nil {
-		return false
-	}
-	return f.IsDir()
+	return form, nil
 }
