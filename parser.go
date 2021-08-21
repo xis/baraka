c3fc3479e047aab2177bc95a96befaabcaf9dfd1
xis_xi <fopeak@gmail.com>
2021-08-21 17:06:56 +0300

    parser: delete available slice mechanism

--- parser.go
+++ parser.go
@@ -12,7 +12,6 @@ import (
 const defaultMaxFileSize = 10 << 20
 const defaultMaxParseCount = 20
 const defaultMaxFileCount = 0
-const defaultMaxAvailableSlice = 2
 
 var errMaxFileCountExceeded = errors.New("max file count to save exceeded")
 var errExtensionNotFound = errors.New("can't detect file's extension")
@@ -26,10 +25,9 @@ type Parser struct {
 
 // ParserOptions contains parser's options about parsing.
 type ParserOptions struct {
-	MaxFileSize       int
-	MaxFileCount      int
-	MaxParseCount     int
-	MaxAvailableSlice int
+	MaxFileSize   int
+	MaxFileCount  int
+	MaxParseCount int
 }
 
 // NewParser creates a new Parser.
@@ -42,10 +40,9 @@ func NewParser(options ParserOptions) *Parser {
 // DefaultParser creates a new parser with the default settings
 func DefaultParser() *Parser {
 	options := ParserOptions{
-		MaxFileSize:       defaultMaxFileSize,
-		MaxFileCount:      defaultMaxFileCount,
-		MaxParseCount:     defaultMaxParseCount,
-		MaxAvailableSlice: defaultMaxAvailableSlice,
+		MaxFileSize:   defaultMaxFileSize,
+		MaxFileCount:  defaultMaxFileCount,
+		MaxParseCount: defaultMaxParseCount,
 	}
 	return NewParser(options)
 }
@@ -65,10 +62,6 @@ func (parser *Parser) SetInspector(inspector Inspector) *Parser {
 // Parse parses the http request with the multipart.Reader.
 // reads parts inside the loop which iterates up to MaxParseCount at most.
 // creates a []byte (buf) which gonna contain the part data.
-// Parse sometimes not creating buf if there is a available buf in availableSlices.
-// it gets a buf from availableSlices and use it.
-// availableSlices fed by filter function,
-// if part can't pass through the filter function, reserved buf for the part gets into availableSlices.
 func (parser *Parser) Parse(r *http.Request) (*Request, error) {
 	reader, err := r.MultipartReader()
 	if err != nil {
@@ -77,7 +70,6 @@ func (parser *Parser) Parse(r *http.Request) (*Request, error) {
 
 	parts := make(map[string][]*Part)
 	request := NewRequest(parts)
-	availableSlices := make([][]byte, 0, parser.Options.MaxAvailableSlice)
 
 	for parseCount := 0; parseCount < parser.Options.MaxParseCount; parseCount++ {
 		part, err := reader.NextPart()
@@ -87,13 +79,8 @@ func (parser *Parser) Parse(r *http.Request) (*Request, error) {
 			}
 			return nil, err
 		}
-		var buf []byte
-		if len(availableSlices) > 0 {
-			buf, availableSlices = availableSlices[len(availableSlices)-1], availableSlices[:len(availableSlices)-1]
-		} else {
-			buf = make([]byte, 0)
-		}
 
+		var buf []byte
 		partBuffer := bytes.NewBuffer(buf)
 		partBuffer.ReadFrom(io.LimitReader(part, int64(parser.Options.MaxFileSize)))
 		part.Close()
@@ -123,13 +110,10 @@ func (parser *Parser) Parse(r *http.Request) (*Request, error) {
 			// execute filter function
 			ok := parser.Filter.Filter(&p)
 			if !ok {
-				if len(availableSlices) <= parser.Options.MaxAvailableSlice {
-					// appending []byte to availableSlices to reuse it
-					availableSlices = append(availableSlices, partBytes[:0])
-				}
 				continue
 			}
 		}
+
 		request.parts[part.FormName()] = append(request.parts[part.FormName()], &p)
 		if len := len(request.parts); len == parser.Options.MaxFileCount && len != 0 {
 			return nil, errMaxFileCountExceeded
