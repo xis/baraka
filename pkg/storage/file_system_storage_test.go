73ba53d000bb864fc1caa0739cc92bd1d47d3605
xis_xi <fopeak@gmail.com>
2022-04-16 22:21:17 +0300

    storage: improve tests

--- pkg/storage/file_system_storage_test.go
+++ pkg/storage/file_system_storage_test.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"os"
+	"syscall"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -35,8 +36,17 @@ func (suite *FilesystemStorageTestSuite) TestStoreButNestedAndAlreadyExists() {
 	err := os.MkdirAll("./testdata/space/star", os.ModeSticky|os.ModePerm)
 	suite.NoError(err)
 
-	err = suite.Storage.Store("/something/something/something.txt", []byte("something"))
+	err = suite.Storage.Store("/space/star/something.txt", []byte("something"))
 	suite.NoError(err)
+}
+
+func (suite *FilesystemStorageTestSuite) TestStoreButPathIsAFile() {
+	err := os.WriteFile("./testdata/space/nebula", []byte("something"), 0666)
+	suite.NoError(err)
+
+	err = suite.Storage.Store("/space/nebula/something.txt", []byte("something"))
+
+	suite.ErrorIs(err, syscall.ENOTDIR)
 }
 
 func (suite *FilesystemStorageTestSuite) TearDownSuite() {
