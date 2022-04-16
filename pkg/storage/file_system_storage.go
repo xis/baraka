a27d608162c09a9da332f7c843df3b6ab5872774
xis_xi <fopeak@gmail.com>
2022-04-16 22:15:06 +0300

    storage: delete is dir check

--- pkg/storage/file_system_storage.go
+++ pkg/storage/file_system_storage.go
@@ -22,23 +22,10 @@ func (s FilesystemStorage) Store(path string, data []byte) error {
 	path = filepath.Join(s.Path, path)
 	dirName := filepath.Dir(path)
 
-	if !isDir(dirName) {
-		err := os.MkdirAll(dirName, os.ModeSticky|os.ModePerm)
-		if err != nil {
-			return err
-		}
+	err := os.MkdirAll(dirName, os.ModeSticky|os.ModePerm)
+	if err != nil {
+		return err
 	}
 
 	return os.WriteFile(path, data, 0666)
-}
-
-// isDir checks if the path is a directory and exists
-func isDir(path string) bool {
-	f, e := os.Stat(path)
-
-	if e != nil {
-		return false
-	}
-
-	return f.IsDir()
 }
